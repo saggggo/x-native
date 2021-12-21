7d06defa98c290571f3d2dc7fe4bf9eb9ad13edc
Saggggo <sagoryota@gmail.com>
2021-12-21 23:20:54 +0900

    update

--- lib/pages/nearby/nearby.dart
+++ lib/pages/nearby/nearby.dart
@@ -1,51 +1,38 @@
 import 'dart:math';
 
 import 'dart:async';
+import 'dart:typed_data';
+import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter/rendering.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
 import '../../components/loading.dart';
+import 'package:geohashlib/geohashlib.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class NearbyPageNavigationContents extends StatelessWidget {
-  bool tf = false;
-  NearbyPageNavigationContents(this.tf);
+// Init firestore and geoFlutterFire
 
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: [
-        CupertinoSwitch(
-          value: tf,
-          onChanged: (val) {
-            if (val) {
-              Permission.location.status.then((p) {
-                print(p);
-                if (p.isDenied) {
-                  Permission.location.request().then((permission) {
-                    print(permission);
-                    if (permission.isPermanentlyDenied) {
-                      openAppSettings();
-                    }
-                  });
-                }
-                tf = true;
-              });
-            } else {
-              tf = false;
-            }
-          },
-        )
-      ],
-    );
+class Spot {
+  final GeoHash geohash;
+  final Latitude lat;
+  final Longitude lon;
+  final Timestamp timestamp;
+
+  Spot(this.geohash, this.lat, this.lon, this.timestamp);
+  static Spot fromMap(Map<String, dynamic> obj) {
+    return new Spot(obj["geohash"], obj["lat"], obj["lng"], obj["timestamp"]);
   }
 }
+
+final _firestore = FirebaseFirestore.instance;
 
 class _NearbyPageState extends State<NearbyPage> {
   bool tf = false;
   bool current = false;
   bool loop = true;
+  bool didMapCreated = false;
+  bool didStyleLoaded = false;
   UserLocation? lastLocation;
   MapboxMapController? mController;
   MyLocationTrackingMode _myLocationTrackingMode = MyLocationTrackingMode.None;
@@ -53,8 +40,8 @@ class _NearbyPageState extends State<NearbyPage> {
 
   void renderer() {
     if (mController != null && loop) {
-      mController!
-          .getMetersPerPixelAtLatitude(this.lastLocation!.position.latitude)
+      mController
+          ?.getMetersPerPixelAtLatitude(this.lastLocation!.position.latitude)
           .then((unit) {
         return mController!.updateCircle(
             userCircle!, CircleOptions(circleRadius: 1000 / unit));
@@ -66,25 +53,48 @@ class _NearbyPageState extends State<NearbyPage> {
     }
   }
 
-  void _onMapCreated(MapboxMapController mc) {
+  void _onMapCreated(MapboxMapController mc) async {
     print("map is created");
     mController = mc;
-    this.renderer();
+    mController?.onSymbolTapped.add((Symbol sym) {
+      print("symbol tapped: " + sym.id);
+    });
     setState(() {
       _myLocationTrackingMode = MyLocationTrackingMode.Tracking;
     });
+    final ByteData bytes =
+        await rootBundle.load("assets/img/location-outline.png");
+    final Uint8List list = bytes.buffer.asUint8List();
+    await mController?.addImage("spot", list);
+    if (didStyleLoaded) {
+      renderer();
+    }
+    didMapCreated = true;
+  }
+
+  void _onStyleLoadedCallback() {
+    print("map style loaded");
+    if (didMapCreated) {
+      renderer();
+    }
+    didStyleLoaded = true;
   }
 
   void _onMapClick(Point<double> p, LatLng lg) {
     print("map clicked: " + p.toString() + "," + lg.toString());
   }
 
-  void _onStyleLoadedCallback() {
-    print("map style loaded");
-  }
-
   void _onUserLocationUpdated(UserLocation location) {
     lastLocation = location;
+    print(distanceBetween(
+        geoPointForDouble(
+            lastLocation!.position.latitude, lastLocation!.position.longitude),
+        geoPointForDouble(
+            location.position.latitude, location.position.longitude)));
+    if (location.horizontalAccuracy != null &&
+        100 > location.horizontalAccuracy!) {
+      lastLocation = location;
+    }
 
     if (mController != null && location.heading?.trueHeading != null) {
       mController!
@@ -131,16 +141,16 @@ class _NearbyPageState extends State<NearbyPage> {
 
   @override
   Widget build(BuildContext context) {
+    double deviceWidth = MediaQuery.of(context).size.width;
+
     return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        trailing: NearbyPageNavigationContents(tf),
-      ),
       child: SafeArea(
+        top: false,
         child: Center(
           child: Container(
             color: Color(0xFFFFFFFF),
             child: Stack(
-              alignment: const Alignment(0.9, 0.9),
+              // alignment: const Alignment(0, 0.9),
               children: [
                 MapboxMap(
                   // styleString: MapboxStyles.DARK,
@@ -172,25 +182,80 @@ class _NearbyPageState extends State<NearbyPage> {
                   attributionButtonMargins: Point(-50, -50), // 画面外
                   logoViewMargins: Point(-50, -50), // 画面外
                 ),
-                Container(
-                  width: 40,
-                  height: 40,
-                  color: Color(0xFFFFFFFF),
-                  child: CupertinoButton(
-                    child: Align(
-                      child: Icon(
-                        CupertinoIcons.checkmark,
-                        color: Color(0xFF000000),
+                SafeArea(
+                  child: Container(
+                    alignment: Alignment.topCenter,
+                    child: Container(
+                      width: deviceWidth * .8,
+                      height: 45,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                          color: Color(0xFFFFFFFF),
+                          border: Border.all(color: Color(0xFFEEEEEE)),
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Color(0xFF333333),
+                              offset: Offset(3, 3),
+                              blurRadius: 3.0,
+                              spreadRadius: 1.0,
+                            ),
+                          ]),
+                      child: CupertinoButton(
+                        child: Align(
+                          child: Icon(
+                            CupertinoIcons.checkmark,
+                            color: Color(0xFF000000),
+                          ),
+                        ),
+                        onPressed: () {
+                          if (lastLocation != null) {
+                            var areas = geohashQueryBounds(
+                                geoPointForDouble(
+                                    lastLocation!.position.latitude,
+                                    lastLocation!.position.longitude),
+                                500);
+                            Future.forEach<Map<String, String>>(areas, (area) {
+                              return _firestore
+                                  .collection("spots")
+                                  .orderBy('geohash')
+                                  .startAt([area["start"]])
+                                  .endAt([area["end"]])
+                                  .get()
+                                  .then((res) {
+                                    for (var doc in res.docs) {
+                                      var sp = Spot.fromMap(doc.data());
+                                      var dist = distanceBetween(
+                                          geoPointForDouble(sp.lat, sp.lon),
+                                          geoPointForDouble(
+                                              lastLocation!.position.latitude,
+                                              lastLocation!
+                                                  .position.longitude));
+                                      print(dist);
+                                      if (500 > dist) {
+                                        var opt = SymbolOptions.defaultOptions;
+                                        print(opt.iconSize);
+                                        mController?.addSymbol(opt.copyWith(
+                                            SymbolOptions(
+                                                iconImage: 'spot',
+                                                iconSize: .08,
+                                                geometry:
+                                                    LatLng(sp.lat, sp.lon))));
+                                        print("lon: " +
+                                            sp.lon.toString() +
+                                            " lat: " +
+                                            sp.lat.toString());
+                                        print(sp);
+                                      }
+                                    }
+                                  });
+                            });
+                          }
+                        },
                       ),
                     ),
-                    onPressed: () {
-                      setState(() {
-                        _myLocationTrackingMode =
-                            MyLocationTrackingMode.Tracking;
-                      });
-                    },
                   ),
-                )
+                ),
               ],
             ),
           ),
