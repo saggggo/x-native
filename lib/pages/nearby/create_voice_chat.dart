5a4d3ab889fd10e5fda616f9ac3c7fef736dbbb6
Saggggo <sagoryota@gmail.com>
2022-01-09 19:34:40 +0900

    update

--- lib/pages/nearby/create_voice_chat.dart
+++ lib/pages/nearby/create_voice_chat.dart
@@ -4,7 +4,6 @@ import 'package:provider/provider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import '../../api/firestore.dart';
 import '../../components/loading.dart';
-import "../../utils/types.dart";
 
 final _firestore = FirebaseFirestore.instance;
 
